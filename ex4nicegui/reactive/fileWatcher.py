7f9408b2d7bc65229a820b9a0217ad10be98323a
CrystalWindSnake <568166495@qq.com>
2023-08-23 14:28:04 +0800

    error resetart server

--- ex4nicegui/reactive/fileWatcher.py
+++ ex4nicegui/reactive/fileWatcher.py
@@ -15,20 +15,26 @@ class FilesWatcher:
         self.__stop_event = asyncio.Event()
 
         async def watch_fn():
-            async for file in awatch(
-                paths, stop_event=self.__stop_event, recursive=recursive
-            ):
-                for fn in self.callbacks:
-                    fn(file)
+            try:
+                async for file in awatch(
+                    paths, stop_event=self.__stop_event, recursive=recursive
+                ):
+                    for fn in self.callbacks:
+                        fn(file)
+            except RuntimeError as e:
+                return
 
         if asyncio.get_event_loop().is_running():
             asyncio.get_event_loop().create_task(watch_fn())
         else:
 
-            def on_app_startup():
+            @app.on_startup
+            def _():
                 asyncio.get_event_loop().create_task(watch_fn())
 
-            app.on_startup(on_app_startup)
+        @app.on_shutdown
+        def _():
+            self.stop()
 
     def stop(self):
         self.__stop_event.set()
