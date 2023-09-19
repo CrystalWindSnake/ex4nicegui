01fffbff47766b4a85dd2150dede3c674343e8e9
CrystalWindSnake <568166495@qq.com>
2023-09-20 01:25:28 +0800

    feat:bi text

--- ex4nicegui/bi/__init__.py
+++ ex4nicegui/bi/__init__.py
@@ -1,3 +1,9 @@
 from .index import data_source
+from .elements.text import ui_title, ui_header, ui_subheader
 
-__all__ = ["data_source"]
+__all__ = [
+    "data_source",
+    "ui_title",
+    "ui_header",
+    "ui_subheader",
+]
