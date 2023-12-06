82861459c1c3eb99533db59b3821de248ea933ef
CrystalWindSnake <568166495@qq.com>
2023-12-06 20:59:22 +0800

    table

--- ex4nicegui/utils/common.py
+++ ex4nicegui/utils/common.py
@@ -11,7 +11,20 @@ def convert_dataframe(df):
 
     assert isinstance(df, pd.DataFrame)
     with pd.option_context("mode.chained_assignment", None):
-        for col in df.select_dtypes(["datetime"]).columns:
-            df[col] = df[col].dt.strftime("%Y-%m-%d")
+        date_cols = df.columns[df.dtypes == "datetime64[ns]"]
+        time_cols = df.columns[df.dtypes == "timedelta64[ns]"]
+        complex_cols = df.columns[df.dtypes == "complex128"]
+        period_cols = df.columns[df.dtypes == "period[M]"]
+        if (
+            len(date_cols) != 0
+            or len(time_cols) != 0
+            or len(complex_cols) != 0
+            or len(period_cols) != 0
+        ):
+            df = df.copy()
+            df[date_cols] = df[date_cols].astype(str)
+            df[time_cols] = df[time_cols].astype(str)
+            df[complex_cols] = df[complex_cols].astype(str)
+            df[period_cols] = df[period_cols].astype(str)
 
     return df
