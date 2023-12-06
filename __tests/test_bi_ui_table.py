bf0799481d30f05f36ed8a29c989c8506b4120cb
CrystalWindSnake <568166495@qq.com>
2023-12-06 21:01:56 +0800

    test

---
+++ __tests/test_bi_ui_table.py
@@ -0,0 +1,23 @@
+from nicegui import ui
+from .screen import ScreenPage
+import pandas as pd
+
+from ex4nicegui import bi
+from .utils import set_test_id, TableUtils
+
+
+def test_base(page: ScreenPage, page_path: str):
+    @ui.page(page_path)
+    def _():
+        data = pd.DataFrame({"name": ["f", "a", "c", "b"], "age": [1, 2, 3, 1]})
+        df = pd.DataFrame(data)
+
+        source = bi.data_source(df)
+
+        set_test_id(source.ui_table(), "target")
+
+    page.open(page_path)
+
+    target = TableUtils(page, "target")
+
+    target.expect_cell_to_be_visible(list("facb"))
