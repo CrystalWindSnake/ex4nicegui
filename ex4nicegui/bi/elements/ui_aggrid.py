2312a702590fcb4056013c57e98e24c244d39920
CrystalWindSnake <568166495@qq.com>
2023-12-08 22:29:57 +0800

    feat

--- ex4nicegui/bi/elements/ui_aggrid.py
+++ ex4nicegui/bi/elements/ui_aggrid.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Any, Callable, Optional, Union
+from typing import TYPE_CHECKING, Any, Callable, Dict, Optional, Union
 from nicegui import ui
 from ex4nicegui.bi.dataSource import DataSource
 from .models import UiResult
@@ -20,17 +20,44 @@ class AggridResult(UiResult[ui.aggrid]):
         self.table_update()
 
 
+def _merge_options(fixed_options: Dict, new_options: Dict):
+    columnDefs = new_options["columnDefs"]  # type:list[dict]
+    new_options.update(fixed_options)
+
+    if "columnDefs" in fixed_options:
+        fixed_columnDefs_map = {
+            col_info["field"]: col_info
+            for col_info in fixed_options["columnDefs"]
+            if "field" in col_info
+        }
+
+        for colDef in columnDefs:
+            info = fixed_columnDefs_map.get(colDef["field"])
+            if info:
+                colDef.update(info)
+
+        new_options.update({"columnDefs": columnDefs})
+
+    return new_options
+
+
 def ui_aggrid(
     self: DataSourceFacade,
+    options: Optional[Dict] = None,
     **kwargs,
 ):
+    fixed_options = options or {}
+    fixed_options.pop("rowData", None)
+
     kwargs.update({"options": {}})
 
     cp = ui.aggrid(**kwargs)
 
     def on_source_update():
         data = self._dataSource.get_filtered_data(cp)
-        cp._props["options"] = self._dataSource._idataSource.get_aggrid_options(data)
+        new_opts = self._dataSource._idataSource.get_aggrid_options(data)
+        options = _merge_options(fixed_options, new_opts)
+        cp._props["options"] = options
         cp.update()
 
     info = self._dataSource._register_component(cp.id, on_source_update)
