e5c33de6090290ea3d89d3952759b5d3bafdd566
CrystalWindSnake <36034954+CrystalWindSnake@users.noreply.github.com>
2023-12-14 00:07:52 +0800

    fix rxui echarts click event error (#76)

--- __tests/test_echarts.py
+++ __tests/test_echarts.py
@@ -6,7 +6,9 @@ from .screen import ScreenPage
 from .utils import EChartsUtils, set_test_id
 
 
-def test_chart_display(page: ScreenPage, page_path: str):
+def test_chart_display(
+    page: ScreenPage, page_path: str
+):
     @ui.page(page_path)
     def _():
         ins = rxui.echarts(
@@ -14,10 +16,31 @@ def test_chart_display(page: ScreenPage, page_path: str):
                 "title": {"text": "echart title"},
                 "xAxis": {
                     "type": "category",
-                    "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+                    "data": [
+                        "Mon",
+                        "Tue",
+                        "Wed",
+                        "Thu",
+                        "Fri",
+                        "Sat",
+                        "Sun",
+                    ],
                 },
                 "yAxis": {"type": "value"},
-                "series": [{"data": [120, 200, 150, 80, 70, 110, 130], "type": "bar"}],
+                "series": [
+                    {
+                        "data": [
+                            120,
+                            200,
+                            150,
+                            80,
+                            70,
+                            110,
+                            130,
+                        ],
+                        "type": "bar",
+                    }
+                ],
             }
         )
 
@@ -32,7 +55,9 @@ def test_chart_display(page: ScreenPage, page_path: str):
     target.assert_chart_exists()
 
 
-def test_js_function_opt(page: ScreenPage, page_path: str):
+def test_js_function_opt(
+    page: ScreenPage, page_path: str
+):
     r_unit = to_ref("kg")
 
     yAxis_formatter = ref_computed(
@@ -46,13 +71,36 @@ def test_js_function_opt(page: ScreenPage, page_path: str):
         lambda: {
             "xAxis": {
                 "type": "category",
-                "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+                "data": [
+                    "Mon",
+                    "Tue",
+                    "Wed",
+                    "Thu",
+                    "Fri",
+                    "Sat",
+                    "Sun",
+                ],
             },
             "yAxis": {
                 "type": "value",
-                "axisLabel": {":formatter": yAxis_formatter.value},
+                "axisLabel": {
+                    ":formatter": yAxis_formatter.value
+                },
             },
-            "series": [{"data": [120, 200, 150, 80, 70, 110, 130], "type": "bar"}],
+            "series": [
+                {
+                    "data": [
+                        120,
+                        200,
+                        150,
+                        80,
+                        70,
+                        110,
+                        130,
+                    ],
+                    "type": "bar",
+                }
+            ],
         }
     )
 
@@ -70,10 +118,15 @@ def test_js_function_opt(page: ScreenPage, page_path: str):
 
     opts = target.get_options()
 
-    assert "formatter" in opts["yAxis"][0]["axisLabel"]
+    assert (
+        "formatter"
+        in opts["yAxis"][0]["axisLabel"]
+    )
 
 
-def test_pyecharts(page: ScreenPage, page_path: str):
+def test_pyecharts(
+    page: ScreenPage, page_path: str
+):
     from pyecharts import options as opts
     from pyecharts.charts import Bar
     from pyecharts.commons import utils
@@ -82,7 +135,17 @@ def test_pyecharts(page: ScreenPage, page_path: str):
     def _():
         c = (
             Bar()
-            .add_xaxis(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"])
+            .add_xaxis(
+                [
+                    "Mon",
+                    "Tue",
+                    "Wed",
+                    "Thu",
+                    "Fri",
+                    "Sat",
+                    "Sun",
+                ]
+            )
             .add_yaxis(
                 "商家A",
                 [120, 200, 150, 80, 70, 110, 130],
@@ -112,4 +175,102 @@ def test_pyecharts(page: ScreenPage, page_path: str):
 
     opts = target.get_options()
 
-    assert "formatter" in opts["yAxis"][0]["axisLabel"]
+    assert (
+        "formatter"
+        in opts["yAxis"][0]["axisLabel"]
+    )
+
+
+def test_click_event(
+    page: ScreenPage, page_path: str
+):
+    click_data = {}
+    hover_data = {}
+
+    @ui.page(page_path)
+    def _():
+        opts = {
+            "xAxis": {"type": "value"},
+            "yAxis": {
+                "type": "category",
+                "data": ["A", "B"],
+                "inverse": True,
+            },
+            "legend": {
+                "textStyle": {"color": "gray"}
+            },
+            "series": [
+                {
+                    "type": "bar",
+                    "name": "Alpha",
+                    "data": [0.1, 0.2],
+                },
+                {
+                    "type": "bar",
+                    "name": "Beta",
+                    "data": [0.3, 0.4],
+                },
+            ],
+        }
+
+        ins = rxui.echarts(opts)
+
+        def onclick_series(
+            e: rxui.echarts.EChartsMouseEventArguments,
+        ):
+            click_data[
+                "seriesName"
+            ] = e.seriesName
+
+        ins.on("click", onclick_series)
+
+        def on_mouser_over(
+            e: rxui.echarts.EChartsMouseEventArguments,
+        ):
+            hover_data[
+                "seriesName"
+            ] = e.seriesName
+
+        ins.on(
+            "mouseover",
+            on_mouser_over,
+            query={"seriesName": "Beta"},
+        )
+
+        set_test_id(ins, "target")
+
+    page.open(page_path)
+
+    target = EChartsUtils(page, "target")
+
+    page.wait(1000)
+
+    target.click_series(
+        0.05, "A", y_position_offset=-8
+    )
+
+    page.wait(1000)
+
+    assert click_data["seriesName"] == "Alpha"
+
+    target.mouse_hover_series(
+        0.05,
+        "B",
+        color="Alpha",
+        y_position_offset=-8,
+    )
+
+    page.wait(1000)
+
+    assert "seriesName" not in hover_data
+
+    target.mouse_hover_series(
+        0.05,
+        "B",
+        color="Beta",
+        y_position_offset=+8,
+    )
+
+    page.wait(1000)
+
+    assert hover_data["seriesName"] == "Beta"
--- __tests/utils.py
+++ __tests/utils.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
 from typing import List, Optional, Any, Callable
-from playwright.sync_api import expect
+from playwright.sync_api import expect, Locator
 from .screen import ScreenPage
 from nicegui import ui
 from typing_extensions import Protocol, Self
 
-ui.element().props
-
 
 class IPropsAble(Protocol):
-    def props(self, add: Optional[str] = None, *, remove: Optional[str] = None) -> Self:
+    def props(
+        self,
+        add: Optional[str] = None,
+        *,
+        remove: Optional[str] = None,
+    ) -> Self:
         ...
 
 
@@ -19,7 +22,9 @@ def set_test_id(element: IPropsAble, id: str):
 
 
 class fn:
-    def __init__(self, fn: Optional[Callable] = None) -> None:
+    def __init__(
+        self, fn: Optional[Callable] = None
+    ) -> None:
         self._fn = fn
         self._called_count = 0
 
@@ -34,7 +39,9 @@ class fn:
     def toHaveBeenCalled(self):
         return self.calledTimes > 0
 
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
+    def __call__(
+        self, *args: Any, **kwds: Any
+    ) -> Any:
         if self._fn is None:
             pass
             self._called_count += 1
@@ -47,14 +54,28 @@ class fn:
         return result
 
 
+def tran_str(value):
+    if isinstance(value, str):
+        return f"'{value}'"
+    return value
+
+
 class SelectUtils:
-    def __init__(self, screen_page: ScreenPage, test_id: str) -> None:
+    def __init__(
+        self,
+        screen_page: ScreenPage,
+        test_id: str,
+    ) -> None:
         self.page = screen_page._page
         self.test_id = test_id
-        self.target_locator = self.page.get_by_test_id(test_id)
+        self.target_locator = (
+            self.page.get_by_test_id(test_id)
+        )
 
     def click(self):
-        self.target_locator.click(position={"x": 5, "y": 5})
+        self.target_locator.click(
+            position={"x": 5, "y": 5}
+        )
 
     def get_options_values(self):
         return self.page.locator(
@@ -62,18 +83,24 @@ class SelectUtils:
         ).all_inner_texts()
 
     def click_cancel(self):
-        self.page.get_by_role("button", name="cancel").click()
+        self.page.get_by_role(
+            "button", name="cancel"
+        ).click()
 
     def click_and_select(self, value: str):
         self.click()
         self.page.wait_for_timeout(500)
-        self.page.get_by_role("option", name=value).click()
+        self.page.get_by_role(
+            "option", name=value
+        ).click()
 
     def get_input_value(self):
         return self.target_locator.input_value()
 
     def get_selected_values(self):
-        return self.target_locator.locator(".q-chip__content").all_inner_texts()
+        return self.target_locator.locator(
+            ".q-chip__content"
+        ).all_inner_texts()
 
     def input_and_enter(self, text: str):
         self.target_locator.type(text)
@@ -82,35 +109,57 @@ class SelectUtils:
 
 
 class RadioUtils:
-    def __init__(self, screen_page: ScreenPage, test_id: str) -> None:
+    def __init__(
+        self,
+        screen_page: ScreenPage,
+        test_id: str,
+    ) -> None:
         self.page = screen_page._page
         self.test_id = test_id
-        self.target_locator = self.page.get_by_test_id(test_id)
+        self.target_locator = (
+            self.page.get_by_test_id(test_id)
+        )
 
     def expect_to_be_visible(self):
-        expect(self.target_locator).to_be_visible()
+        expect(
+            self.target_locator
+        ).to_be_visible()
 
     def is_checked_by_label(self, label: str):
-        return self.target_locator.get_by_label(label).is_checked()
+        return self.target_locator.get_by_label(
+            label
+        ).is_checked()
 
     def check_by_label(self, label: str):
         # self.target_locator.click(f"text={label}")
 
-        self.target_locator.get_by_label(label).click()
+        self.target_locator.get_by_label(
+            label
+        ).click()
         return
 
     def get_all_labels(self):
-        return self.target_locator.locator(".q-radio__label").all_inner_texts()
+        return self.target_locator.locator(
+            ".q-radio__label"
+        ).all_inner_texts()
 
 
 class EChartsUtils:
-    def __init__(self, screen_page: ScreenPage, test_id: str) -> None:
+    def __init__(
+        self,
+        screen_page: ScreenPage,
+        test_id: str,
+    ) -> None:
         self.page = screen_page._page
         self.test_id = test_id
-        self.target_locator = self.page.get_by_test_id(test_id)
+        self.target_locator = (
+            self.page.get_by_test_id(test_id)
+        )
 
     def assert_chart_exists(self):
-        attr = self.target_locator.get_attribute("_echarts_instance_")
+        attr = self.target_locator.get_attribute(
+            "_echarts_instance_"
+        )
         assert attr is not None
 
     def get_options(self):
@@ -120,30 +169,118 @@ class EChartsUtils:
 
         return opts
 
+    def click_series(
+        self,
+        x_value,
+        y_value,
+        color=None,
+        seriesIndex=0,
+        x_position_offset=0.0,
+        y_position_offset=0.0,
+    ):
+        x, y = EChartsUtils.cal_series_pos(
+            self.target_locator,
+            x_value,
+            y_value,
+            color,
+            seriesIndex,
+        )
+
+        self.page.mouse.click(
+            x + x_position_offset,
+            y + y_position_offset,
+        )
+
+    def mouse_hover_series(
+        self,
+        x_value,
+        y_value,
+        color=None,
+        seriesIndex=0,
+        x_position_offset=0.0,
+        y_position_offset=0.0,
+    ):
+        x, y = EChartsUtils.cal_series_pos(
+            self.target_locator,
+            x_value,
+            y_value,
+            color,
+            seriesIndex,
+        )
+
+        self.page.mouse.move(
+            x + x_position_offset,
+            y + y_position_offset,
+        )
+
+    @staticmethod
+    def cal_series_pos(
+        chartIns_box: Locator,
+        x_value,
+        y_value,
+        color=None,
+        seriesIndex=0,
+    ):
+        finder = ""
+        if color is None:
+            finder = f"{{ seriesIndex: {seriesIndex} }}"
+        else:
+            finder = f"{{ seriesName: {tran_str(color)} }}"
+
+        pos_x, pos_y = chartIns_box.evaluate(
+            f"node => echarts.getInstanceByDom(node).convertToPixel({finder}, [{tran_str(x_value)}, {tran_str(y_value)}])"
+        )
+
+        box = chartIns_box.bounding_box()
+        return (
+            box["x"] + pos_x,
+            box["y"] + pos_y,
+        )
+
 
 class TableUtils:
-    def __init__(self, screen_page: ScreenPage, test_id: str) -> None:
+    def __init__(
+        self,
+        screen_page: ScreenPage,
+        test_id: str,
+    ) -> None:
         self.page = screen_page._page
         self.test_id = test_id
-        self.target_locator = self.page.get_by_test_id(test_id)
+        self.target_locator = (
+            self.page.get_by_test_id(test_id)
+        )
 
-    def expect_cell_to_be_visible(self, cell_values: List):
+    def expect_cell_to_be_visible(
+        self, cell_values: List
+    ):
         for cell_value in cell_values:
             expect(
-                self.target_locator.get_by_role("cell", name=cell_value, exact=True)
+                self.target_locator.get_by_role(
+                    "cell",
+                    name=cell_value,
+                    exact=True,
+                )
             ).to_be_visible()
 
-    def expect_cell_not_to_be_visible(self, cell_values: List):
+    def expect_cell_not_to_be_visible(
+        self, cell_values: List
+    ):
         for cell_value in cell_values:
             expect(
-                self.target_locator.get_by_role("cell", name=cell_value, exact=True)
+                self.target_locator.get_by_role(
+                    "cell",
+                    name=cell_value,
+                    exact=True,
+                )
             ).not_to_be_visible()
 
     def click_checkbox(self, row_values: List):
-        name = " ".join((str(d) for d in row_values))
-        self.target_locator.get_by_role("row", name=name).get_by_role(
-            "checkbox"
-        ).click()
+        name = " ".join(
+            (str(d) for d in row_values)
+        )
+        self.target_locator.get_by_role(
+            "row", name=name
+        ).get_by_role("checkbox").click()
 
     def get_cell_style(self, cell_value: str):
         return self.target_locator.get_by_role(
@@ -152,77 +289,133 @@ class TableUtils:
 
     def is_sortable(self, column_name: str):
         # sortable
-        return self.target_locator.locator("thead th", has_text=column_name).evaluate(
+        return self.target_locator.locator(
+            "thead th", has_text=column_name
+        ).evaluate(
             "node => node.classList.contains('sortable')"
         )
 
 
 class MermaidUtils:
-    def __init__(self, screen_page: ScreenPage, test_id: str) -> None:
+    def __init__(
+        self,
+        screen_page: ScreenPage,
+        test_id: str,
+    ) -> None:
         self.page = screen_page._page
         self.test_id = test_id
-        self.target_locator = self.page.get_by_test_id(test_id)
+        self.target_locator = (
+            self.page.get_by_test_id(test_id)
+        )
 
     def click_node(self, nodeId: str):
-        self.target_locator.get_by_text(nodeId, exact=True).click()
+        self.target_locator.get_by_text(
+            nodeId, exact=True
+        ).click()
 
 
 class AggridUtils:
-    def __init__(self, screen_page: ScreenPage, test_id: str) -> None:
+    def __init__(
+        self,
+        screen_page: ScreenPage,
+        test_id: str,
+    ) -> None:
         self.page = screen_page._page
         self.test_id = test_id
-        self.target_locator = self.page.get_by_test_id(test_id)
+        self.target_locator = (
+            self.page.get_by_test_id(test_id)
+        )
 
-    def expect_cell_to_be_visible(self, cell_values: List):
+    def expect_cell_to_be_visible(
+        self, cell_values: List
+    ):
         for cell_value in cell_values:
             expect(
-                self.target_locator.get_by_role("gridcell", name=cell_value, exact=True)
+                self.target_locator.get_by_role(
+                    "gridcell",
+                    name=cell_value,
+                    exact=True,
+                )
             ).to_be_visible()
 
-    def expect_selection_cell_to_be_visible(self, cell_values: List):
+    def expect_selection_cell_to_be_visible(
+        self, cell_values: List
+    ):
         for cell_value in cell_values:
             expect(
-                self.target_locator.get_by_role("gridcell")
+                self.target_locator.get_by_role(
+                    "gridcell"
+                )
                 .locator("div")
                 .filter(has_text="a")
             ).to_be_visible()
 
-    def expect_cell_not_to_be_visible(self, cell_values: List):
+    def expect_cell_not_to_be_visible(
+        self, cell_values: List
+    ):
         for cell_value in cell_values:
             expect(
-                self.target_locator.get_by_role("gridcell", name=cell_value, exact=True)
+                self.target_locator.get_by_role(
+                    "gridcell",
+                    name=cell_value,
+                    exact=True,
+                )
             ).not_to_be_visible()
 
     def get_rows(
         self,
     ):
-        return self.target_locator.locator(".ag-body-viewport").get_by_role("row").all()
+        return (
+            self.target_locator.locator(
+                ".ag-body-viewport"
+            )
+            .get_by_role("row")
+            .all()
+        )
 
     def get_data(self):
         return [
-            row.get_by_role("gridcell").all_inner_texts() for row in self.get_rows()
+            row.get_by_role(
+                "gridcell"
+            ).all_inner_texts()
+            for row in self.get_rows()
         ]
 
     def get_cells(self):
         rows = self.get_rows()
-        return [r.get_by_role("gridcell").all() for r in rows]
+        return [
+            r.get_by_role("gridcell").all()
+            for r in rows
+        ]
 
 
 class ButtonUtils:
-    def __init__(self, screen_page: ScreenPage, test_id: str) -> None:
+    def __init__(
+        self,
+        screen_page: ScreenPage,
+        test_id: str,
+    ) -> None:
         self.page = screen_page._page
         self.test_id = test_id
-        self.target_locator = self.page.get_by_test_id(test_id)
+        self.target_locator = (
+            self.page.get_by_test_id(test_id)
+        )
 
     def click(self):
         self.target_locator.click()
 
 
 class LabelUtils:
-    def __init__(self, screen_page: ScreenPage, test_id: str) -> None:
+    def __init__(
+        self,
+        screen_page: ScreenPage,
+        test_id: str,
+    ) -> None:
         self.page = screen_page._page
         self.test_id = test_id
-        self.target_locator = self.page.get_by_test_id(test_id)
+        self.target_locator = (
+            self.page.get_by_test_id(test_id)
+        )
 
     def get_text(self):
         return self.target_locator.inner_text()
