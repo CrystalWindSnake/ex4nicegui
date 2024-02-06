ab8d4d4b8e2ff276ca829f26dedfd613e7176530
CrystalWindSnake <568166495@qq.com>
2024-02-06 17:36:45 +0800

    fix

--- ex4nicegui/utils/scheduler.py
+++ ex4nicegui/utils/scheduler.py
@@ -2,18 +2,21 @@ from signe import utils as signe_utils
 import asyncio
 
 
-class EventDelayExecutionScheduler(signe_utils.ExecutionScheduler):
+class EventDelayExecutionScheduler(signe_utils.BatchExecutionScheduler):
     def __init__(self) -> None:
         super().__init__()
         self.__has_callback = False
 
     def run(self):
         if not self.__has_callback:
-            asyncio.get_running_loop().call_soon(self.real_run)
+            try:
+                asyncio.get_running_loop().call_soon(self.real_run)
+            except RuntimeError:
+                super().run_batch()
             self.__has_callback = True
 
     def real_run(
         self,
     ):
-        super().run()
+        super().run_batch()
         self.__has_callback = False
