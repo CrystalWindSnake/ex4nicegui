f7a5b7639cca5cff5bc288088c88e9f48ee86133
CrystalWindSnake <568166495@qq.com>
2024-02-21 22:47:50 +0800

    fix test

--- __tests/test_select.py
+++ __tests/test_select.py
@@ -206,5 +206,5 @@ def test_opts_value_change_same_time(page: ScreenPage, page_path: str):
     target.expect_to_have_value("a")
 
     button.click()
-    page.wait()
+    page.wait(1000)
     target.expect_to_have_value("m")
