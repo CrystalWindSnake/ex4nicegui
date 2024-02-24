d5f0610f901f80acfd469d6e44cc571ca90abcdf
CrystalWindSnake <568166495@qq.com>
2024-02-25 02:40:27 +0800

    feat

--- ex4nicegui/gsap/__init__.py
+++ ex4nicegui/gsap/__init__.py
@@ -1,1 +1,12 @@
 from .gsap import set_defaults, from_, to, new, run_script
+from .timeline import Timeline as timeline
+
+
+__all__ = [
+    "set_defaults",
+    "from_",
+    "to",
+    "new",
+    "run_script",
+    "timeline",
+]
