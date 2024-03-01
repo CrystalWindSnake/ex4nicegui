a11c36d2cde9c14467f695ca8026df2c679cbd0b
CrystalWindSnake <568166495@qq.com>
2024-03-01 18:49:38 +0800

    test

--- __tests/conftest.py
+++ __tests/conftest.py
@@ -5,7 +5,8 @@ from starlette.routing import Route
 from .screen import Screen
 from nicegui.page import page as ui_page
 from nicegui import Client, app, binding, core
-from nicegui.elements import plotly, pyplot
+
+# from nicegui.elements import plotly, pyplot
 import os
 
 HEADLESS = "GITHUB_ACTION" in os.environ
@@ -16,26 +17,26 @@ def reset_globals(request: pytest.FixtureRequest):
     if "noautofixt" in request.keywords:
         return
 
-    for route in app.routes:
-        if isinstance(route, Route) and route.path.startswith("/_nicegui/auto/static/"):
-            app.remove_route(route.path)
-    for path in {"/"}.union(Client.page_routes.values()):
-        app.remove_route(path)
-    app.openapi_schema = None
-    app.middleware_stack = None
-    app.user_middleware.clear()
-    # NOTE favicon routes must be removed separately because they are not "pages"
-    for route in app.routes:
-        if isinstance(route, Route) and route.path.endswith("/favicon.ico"):
-            app.routes.remove(route)
-    importlib.reload(core)
-    Client.instances.clear()
-    Client.page_routes.clear()
-    app.reset()
-    Client.auto_index_client = Client(ui_page("/"), shared=True).__enter__()  # pylint: disable=unnecessary-dunder-call
-    # NOTE we need to re-add the auto index route because we removed all routes above
-    app.get("/")(Client.auto_index_client.build_response)
-    binding.reset()
+    # for route in app.routes:
+    #     if isinstance(route, Route) and route.path.startswith("/_nicegui/auto/static/"):
+    #         app.remove_route(route.path)
+    # for path in {"/"}.union(Client.page_routes.values()):
+    #     app.remove_route(path)
+    # app.openapi_schema = None
+    # app.middleware_stack = None
+    # app.user_middleware.clear()
+    # # NOTE favicon routes must be removed separately because they are not "pages"
+    # for route in app.routes:
+    #     if isinstance(route, Route) and route.path.endswith("/favicon.ico"):
+    #         app.routes.remove(route)
+    # importlib.reload(core)
+    # Client.instances.clear()
+    # Client.page_routes.clear()
+    # app.reset()
+    # index_client = Client(ui_page("/"), shared=True).__enter__()
+    # app.get("/")(index_client.build_response)
+    # binding.reset()
+    yield
 
 
 @pytest.fixture(scope="session")
