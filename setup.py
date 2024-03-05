ab5519dc27b72f941e030c23701badf24e783f74
CrystalWindSnake <568166495@qq.com>
2024-03-05 21:13:01 +0800

    requirements

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_data_files(base):
 readme = Path("README.en.md").read_text("utf8")
 
 
-requirements = ["signe>=0.4.9", "nicegui>=1.4.0", "typing_extensions", "executing"]
+requirements = ["signe>=0.4.10", "nicegui>=1.4.0", "typing_extensions", "executing"]
 
 test_requirements = ["pytest>=3", "playwright", "pandas", "executing"]
 
