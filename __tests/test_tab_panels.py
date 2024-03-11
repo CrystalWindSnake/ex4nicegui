e494992d315f58a8c460718127c3f878771f3cc4
CrystalWindSnake <568166495@qq.com>
2024-03-11 20:49:02 +0800

    panels test

---
+++ __tests/test_tab_panels.py
@@ -0,0 +1,32 @@
+from ex4nicegui.reactive import rxui
+from nicegui import ui
+from ex4nicegui import to_ref
+from .screen import ScreenPage
+from .utils import InputUtils, set_test_id, BaseUiUtils
+
+
+def test_base(page: ScreenPage, page_path: str):
+    @ui.page(page_path)
+    def _():
+        current = to_ref("a")
+
+        set_test_id(rxui.input(value=current), "input")
+
+        with rxui.tab_panels(current) as panels:
+            with rxui.tab_panel("a"):
+                rxui.label("a page")
+
+            with rxui.tab_panel("b"):
+                rxui.label("b page")
+
+        set_test_id(panels, "panels")
+
+    page.open(page_path)
+
+    input = InputUtils(page, "input")
+    tabs = BaseUiUtils(page, "panels")
+
+    tabs.expect.to_contain_text("a page")
+
+    input.fill_text("b")
+    tabs.expect.to_contain_text("b page")
