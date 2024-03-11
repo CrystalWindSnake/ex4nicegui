99961881e9a34930949afdf61ec9a8fa5527beb1
CrystalWindSnake <568166495@qq.com>
2024-03-11 23:04:02 +0800

    remove log

--- ex4nicegui/reactive/vfor.js
+++ ex4nicegui/reactive/vfor.js
@@ -2,15 +2,11 @@ export default {
     props: { itemIds: Array },
 
     render() {
-        // debugger
         const slotBox = this.$slots.default()
-        console.log(slotBox, this.itemIds);
 
         const slots = this.itemIds.map(({ elementId }) => {
             return slotBox.find(v => v.key === elementId)
         });
-
-        console.log(slots);
 
         return slots
     }
