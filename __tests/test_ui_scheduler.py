e043fa872182bbe3e0f8f942013bfb44c0cb82a4
CrystalWindSnake <568166495@qq.com>
2024-05-15 02:02:28 +0800

    testing

--- __tests/test_ui_scheduler.py
+++ __tests/test_ui_scheduler.py
@@ -1,28 +1,39 @@
-import pytest
-from ex4nicegui import to_ref, ref_computed, effect
+from ex4nicegui import rxui, ref_computed, effect, to_ref
+from nicegui import ui
+from .screen import ScreenPage
+from .utils import InputUtils, LabelUtils, set_test_id
 
 
-@pytest.mark.noautofixt
-def test_when_error():
-    dummy = []
-
-    try:
-        pass
-        s = to_ref(1)
+def test_when_error_in_effect(page: ScreenPage, page_path: str):
+    @ui.page(page_path)
+    def _():
+        text = to_ref("Hello")
+        dummy = to_ref("")
 
         @ref_computed
         def cp():
-            if s.value > 1:
+            if text.value == "foo":
                 raise Exception("")
 
-            return s.value + 1
+            return text.value + " world"
 
         @effect
         def _():
-            dummy.append(cp.value)
+            dummy.value = str(cp.value)
 
-        s.value = 2
-    except Exception:
-        s.value = -1
+        set_test_id(rxui.input(value=text), "input")
+        set_test_id(rxui.label(dummy), "label")
 
-    assert dummy == [2, 0]
+    page.open(page_path)
+    page.wait(600)
+
+    input = InputUtils(page, "input")
+    label = LabelUtils(page, "label")
+
+    label.expect_contain_text("Hello world")
+
+    input.fill_text("foo")
+    label.expect_contain_text("Hello world")
+
+    input.fill_text("bar")
+    label.expect_contain_text("bar world")
