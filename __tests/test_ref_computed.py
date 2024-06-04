b8275a9493b48d90948c5d8a785ca289e264a33a
CrystalWindSnake <568166495@qq.com>
2024-06-05 01:46:05 +0800

    testing

--- __tests/test_ref_computed.py
+++ __tests/test_ref_computed.py
@@ -62,3 +62,26 @@ def test_should_not_destroyed():
     s.add_data()
     s.add_data()
     assert dummy == [0, 1, 2]
+
+
+def test_should_work_with_private_page(self, browser: BrowserManager, page_path: str):
+    class MyState:
+        def __init__(self):
+            self.count = to_ref(0)
+
+        @ref_computed
+        def color(self):
+            return "green" if self.count.value % 2 == 0 else "red"
+
+    state = MyState()
+
+    @ui.page(page_path)
+    def _():
+        rxui.label(state.color)
+        ui.button("reload", on_click=ui.navigate.reload).classes("button")
+
+    page = browser.open(page_path)
+
+    button = page.Button(".button")
+
+    button.click(click_count=10)
