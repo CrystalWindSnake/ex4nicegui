7058081b6841ac79393c28852443cab67de6d296
CrystalWindSnake <568166495@qq.com>
2024-06-13 18:49:31 +0800

    add warning

--- ex4nicegui/gsap/__init__.py
+++ ex4nicegui/gsap/__init__.py
@@ -2,6 +2,17 @@ from .gsap import set_defaults, from_, to, new, run_script
 from .timeline import Timeline as timeline
 
 
+import warnings
+
+RED = "\033[91m"
+RESET = "\033[0m"
+
+warnings.warn(
+    f"{RED}The gsap module is deprecated and will be removed in the next major version.{RESET}",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
 __all__ = [
     "set_defaults",
     "from_",
