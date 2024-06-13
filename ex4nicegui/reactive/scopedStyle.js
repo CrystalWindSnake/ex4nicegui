e1a76ef867a14475a23ed59e23640cf347c25329
CrystalWindSnake <568166495@qq.com>
2024-06-13 16:39:43 +0800

    feat:support for css file

--- ex4nicegui/reactive/scopedStyle.js
+++ ex4nicegui/reactive/scopedStyle.js
@@ -1,6 +1,14 @@
 
+const REGEX_NEWLINE = /[\r\n]/g;
+const REGEX_START_SPACES = /^\s+/gm;
+
 function getClasses(id) {
     return `ex4ng-scoped-style-${id}`;
+}
+
+function removeInvalidChars(str) {
+    str = str.replace(REGEX_START_SPACES, '');
+    return str.replace(REGEX_NEWLINE, '');
 }
 
 
@@ -12,6 +20,7 @@ export default {
         createStyle(id, css) {
             const classes = getClasses(id);
             const target = document.querySelector(`style.${classes}`);
+            css = removeInvalidChars(css);
             if (target) {
                 target.innerHTML = css;
             } else {
