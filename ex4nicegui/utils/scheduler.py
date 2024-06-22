eb505817c9ee502a3058d9abc29b980b0d637882
CrystalWindSnake <568166495@qq.com>
2024-06-22 23:28:20 +0800

    next tick deque

--- ex4nicegui/utils/scheduler.py
+++ ex4nicegui/utils/scheduler.py
@@ -16,12 +16,16 @@ class UiScheduler(signe.ExecutionScheduler):
 
         self._pre_deque: deque[T_JOB_FN] = deque()
         self._post_deque: deque[T_JOB_FN] = deque()
+        self._next_tick_deque: deque[T_JOB_FN] = deque()
 
     def pre_job(self, job: T_JOB_FN):
         self._pre_deque.appendleft(job)
 
     def post_job(self, job: T_JOB_FN):
         self._post_deque.appendleft(job)
+
+    def next_tick_job(self, job: T_JOB_FN):
+        self._next_tick_deque.appendleft(job)
 
     def run(self):
         while self._scheduler_fns:
@@ -31,6 +35,7 @@ class UiScheduler(signe.ExecutionScheduler):
             try:
                 self.run_pre_deque()
                 self.run_post_deque()
+                self.run_next_tick_deque()
                 pass
             except Exception as e:
                 raise e
@@ -45,16 +50,15 @@ class UiScheduler(signe.ExecutionScheduler):
         while self._post_deque:
             self._post_deque.pop()()
 
+    def run_next_tick_deque(self):
+        while self._next_tick_deque:
+            self._next_tick_deque.pop()()
+
 
 @lru_cache(maxsize=1)
 def get_uiScheduler():
     return UiScheduler()
 
 
-def next_tick(job: T_JOB_FN, type: T_JOB_TYPE = "pre"):
-    if type == "pre":
-        get_uiScheduler().pre_job(job)
-    elif type == "post":
-        get_uiScheduler().post_job(job)
-    else:
-        raise ValueError("Invalid job type")
+def next_tick(job: T_JOB_FN):
+    get_uiScheduler().next_tick_job(job)
