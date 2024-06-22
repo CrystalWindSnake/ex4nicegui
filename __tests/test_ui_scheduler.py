c8ca98d7a5ae284f64b629590554fad6a699ffd6
CrystalWindSnake <568166495@qq.com>
2024-06-23 00:26:08 +0800

    testing

--- __tests/test_ui_scheduler.py
+++ __tests/test_ui_scheduler.py
@@ -1,4 +1,4 @@
-from ex4nicegui import rxui, ref_computed, effect, to_ref
+from ex4nicegui import rxui, ref_computed, effect, to_ref, next_tick
 from nicegui import ui
 from .screen import BrowserManager
 
@@ -35,3 +35,37 @@ def test_when_error_in_effect(browser: BrowserManager, page_path: str):
 
     input.fill_text("bar")
     label.expect_contain_text("bar world")
+
+
+def test_next_tick(browser: BrowserManager, page_path: str):
+    @ui.page(page_path)
+    def _():
+        a = to_ref(1)
+        texts = []
+
+        @effect
+        def _():
+            a.value
+            texts.append("effect start")
+
+            @next_tick
+            def _():
+                texts.append("next tick")
+
+            texts.append("effect end")
+
+        def plus_one():
+            a.value += 1
+            lbl_texts.set_text("|".join(texts))
+
+        lbl_texts = ui.label().classes("texts")
+        ui.button("+1", on_click=plus_one).classes("button")
+
+    page = browser.open(page_path)
+    button = page.Button(".button")
+    label = page.Label(".texts")
+
+    button.click()
+    label.expect_contain_text(
+        "effect start|effect end|next tick|effect start|effect end|next tick"
+    )
