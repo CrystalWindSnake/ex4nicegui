afb225dd82ee97a06ecc8d5b4a184c25cbb08465
CrystalWindSnake <568166495@qq.com>
2024-08-05 19:18:13 +0800

    works and testing

--- ex4nicegui/reactive/__init__.py
+++ ex4nicegui/reactive/__init__.py
@@ -63,6 +63,7 @@ from .usePagination import PaginationRef as use_pagination
 from .dropZone.dropZone import use_drag_zone
 from .fileWatcher import FilesWatcher
 from .mermaid.mermaid import Mermaid as mermaid
+from .officials.dialog import DialogBindableUi as dialog
 from .vfor import vfor, VforStore
 from .vmodel import vmodel
 
@@ -124,4 +125,5 @@ __all__ = [
     "pagination",
     "mermaid",
     "chip",
+    "dialog",
 ]
