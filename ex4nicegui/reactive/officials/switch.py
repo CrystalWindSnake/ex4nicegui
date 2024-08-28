91a6ade782605fe3a36153709a4df36b1a478bd2
CrystalWindSnake <568166495@qq.com>
2024-08-28 18:50:04 +0800

    fix switch value

--- ex4nicegui/reactive/officials/switch.py
+++ ex4nicegui/reactive/officials/switch.py
@@ -35,7 +35,6 @@ class SwitchBindableUi(BindableUi[ui.switch]):
         )
 
         value_kws = pc.get_values_kws()
-        value_kws.update({"value": 0 if value is None else value})
 
         element = ui.switch(**value_kws)
         super().__init__(element)  # type: ignore
