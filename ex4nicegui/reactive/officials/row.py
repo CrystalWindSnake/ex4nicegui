97e936cb2478a02c68818eb0c8d6bf25168da2c5
CrystalWindSnake <568166495@qq.com>
2024-09-03 18:02:27 +0800

    row works

--- ex4nicegui/reactive/officials/row.py
+++ ex4nicegui/reactive/officials/row.py
@@ -1,17 +1,24 @@
 from typing import (
     Any,
+    Literal,
+    Optional,
 )
 from ex4nicegui.reactive.services.reactive_service import ParameterClassifier
-from ex4nicegui.utils.signals import (
-    _TMaybeRef as TMaybeRef,
-)
+from ex4nicegui.utils.signals import _TMaybeRef as TMaybeRef, to_value
 from nicegui import ui
 from .base import BindableUi
 
 
 class RowBindableUi(BindableUi[ui.row]):
-    def __init__(self, *, wrap: TMaybeRef[bool] = True) -> None:
-        pc = ParameterClassifier(locals(), maybeRefs=["wrap"], events=[])
+    def __init__(
+        self,
+        *,
+        wrap: TMaybeRef[bool] = True,
+        align_items: Optional[
+            TMaybeRef[Literal["start", "end", "center", "baseline", "stretch"]]
+        ] = None,
+    ) -> None:
+        pc = ParameterClassifier(locals(), maybeRefs=["wrap", "align_items"], events=[])
         element = ui.row(**pc.get_values_kws())
 
         super().__init__(element)
@@ -19,13 +26,19 @@ class RowBindableUi(BindableUi[ui.row]):
             self.bind_prop(key, value)  # type: ignore
 
     def bind_prop(self, prop: str, value: TMaybeRef):
+        print(f"bind_prop: {prop} {value}")
         if prop == "wrap":
             return self.bind_wrap(value)
+        if prop == "align-items":
+            return self.bind_align_items(value)
 
         return super().bind_prop(prop, value)
 
     def bind_wrap(self, value: TMaybeRef):
         self.bind_classes({"wrap": value})
+
+    def bind_align_items(self, value: TMaybeRef):
+        self.bind_classes(lambda: f"items-{to_value(value)}")
 
     def __enter__(self):
         self.element.__enter__()
