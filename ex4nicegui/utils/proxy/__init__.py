798faca543aadd2fe52bd100e03f56249ea8c7c0
CrystalWindSnake <568166495@qq.com>
2024-09-18 22:33:16 +0800

    to value

--- ex4nicegui/utils/proxy/__init__.py
+++ ex4nicegui/utils/proxy/__init__.py
@@ -13,3 +13,9 @@ def to_ref_if_base_type_proxy(obj):
         return obj._ref
     else:
         return obj
+
+def to_value_if_base_type_proxy(obj):
+    if is_base_type_proxy(obj):
+        return obj._ref.value
+    else:
+        return obj
