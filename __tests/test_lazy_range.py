9c7d4b0aedea573c3e5503b3325cf1c01ba79849
CrystalWindSnake <36034954+CrystalWindSnake@users.noreply.github.com>
2024-11-25 16:09:31 +0800

    Add `rxui.lazy_range`  (#270)

---
+++ __tests/test_lazy_range.py
@@ -0,0 +1,82 @@
+from ex4nicegui.reactive import rxui
+from nicegui import ui
+from ex4nicegui import to_ref
+from .screen import BrowserManager, PageUtils
+from typing import Literal
+
+
+def drap_move_and_hold(type: Literal["min", "max"], page: PageUtils, offset_x: int):
+    targets = page.locator(".q-slider__focus-ring")
+    if type == "min":
+        target = targets.first
+    else:
+        target = targets.last
+
+    rect = target.bounding_box()
+    assert rect
+    x = rect["x"] + rect["width"] / 2
+    y = rect["y"] + rect["height"] / 2
+
+    page._page.mouse.move(x, y)
+    page._page.mouse.down()
+    page._page.mouse.move(x + offset_x, y)
+
+
+def mouse_up(
+    page: PageUtils,
+):
+    page._page.mouse.up()
+
+
+def test_ref_value_change(browser: BrowserManager, page_path: str):
+    r_value = to_ref(
+        {
+            "min": 0,
+            "max": 100,
+        }
+    )
+
+    @ui.page(page_path)
+    def _():
+        rxui.lazy_range(min=0, max=100, value=r_value).classes("target")
+        rxui.label(r_value).classes("label")
+
+    page = browser.open(page_path)
+    label = page.Label(".label")
+
+    label.expect_equal_text("{'min': 0, 'max': 100}")
+    drap_move_and_hold("min", page, offset_x=100)
+    label.expect_equal_text("{'min': 0, 'max': 100}")
+
+    mouse_up(page)
+    label.expect_equal_text("{'min': 8, 'max': 100}")
+
+
+def test_on_change_event(browser: BrowserManager, page_path: str):
+    r_value = to_ref(
+        {
+            "min": 0,
+            "max": 100,
+        }
+    )
+
+    @ui.page(page_path)
+    def _():
+        def on_change(e):
+            label.text = str(e.args)
+
+        rxui.lazy_range(min=0, max=100, value=r_value, on_change=on_change).classes(
+            "target"
+        )
+
+        label = ui.label("old").classes("label")
+
+    page = browser.open(page_path)
+    label = page.Label(".label")
+
+    label.expect_equal_text("old")
+    drap_move_and_hold("min", page, offset_x=100)
+    label.expect_equal_text("old")
+
+    mouse_up(page)
+    label.expect_equal_text("{'min': 8, 'max': 100}")
--- ex4nicegui/reactive/officials/range.py
+++ ex4nicegui/reactive/officials/range.py
@@ -1,11 +1,13 @@
 from typing import Any, Callable, Optional, Dict, cast
 
 
+from ex4nicegui.utils.signals import to_value, is_setter_ref
 from ex4nicegui.utils.signals import (
     _TMaybeRef as TMaybeRef,
 )
 
 from nicegui import ui
+from nicegui.events import handle_event
 from ex4nicegui.reactive.base import BindableUi, DisableableMixin
 from ex4nicegui.reactive.services.reactive_service import ParameterClassifier
 from ex4nicegui.reactive.mixins.value_element import ValueElementMixin
@@ -43,3 +45,40 @@ class RangeBindableUi(
     @property
     def value(self):
         return cast(Dict[str, int], self.element.value)
+
+
+class LazyRangeBindableUi(
+    RangeBindableUi,
+):
+    def __init__(
+        self,
+        *,
+        min: TMaybeRef[float],  # pylint: disable=redefined-builtin
+        max: TMaybeRef[float],  # pylint: disable=redefined-builtin
+        step: TMaybeRef[float] = 1.0,
+        value: Optional[TMaybeRef[Dict[str, int]]] = None,
+        on_change: Optional[Callable[..., Any]] = None,
+    ) -> None:
+        """A lazy version of `rxui.range` that only updates the value when the user releases the range.
+
+        Args:
+            min (TMaybeRef[float]): lower bound of the range
+            max (TMaybeRef[float]): upper bound of the range
+            step (TMaybeRef[float], optional): step size. Defaults to 1.0.
+            value (Optional[TMaybeRef[Dict[str, int]]], optional): initial value to set min and max position of the range. Defaults to None.
+            on_change (Optional[Callable[..., Any]], optional): callback which is invoked when the user releases the range. Defaults to None.
+        """
+        super().__init__(
+            min=min,
+            max=max,
+            step=step,
+            value=lambda: to_value(value),  # type: ignore
+        )
+
+        if is_setter_ref(value):
+
+            def new_on_change(e):
+                value.set_value(e.args)  # type: ignore
+                handle_event(on_change, e)
+
+            self.on("change", new_on_change)
